2f8260c6a729ac7385d9c917a33829f14a377171
SudhenduPatni <sudhendu.patni@hotmail.co.uk>
2022-07-17 17:07:30 +0530

    Add project files.

---
+++ FindDatesBetweeenRangeSln/Helper.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace FindDatesBetweeenRangeSln
+{
+    public static class Helper
+    {
+        public enum RecurringType
+        {
+            Daily = 1,
+            Weekly = 2,
+            Monthly = 3,
+            Yearly = 4
+        }
+
+        public static List<DateTime> GetDailyDatesBetweenRange(this DateTime fromDate, DateTime toDate, bool weekdays, int priorDays = 0, int skipDays = 0)
+        {
+            var dates = new List<DateTime>();
+
+            if (weekdays)
+            {
+                for (DateTime date = fromDate; date <= toDate; date = date.AddDays(1))
+                {
+                    if (date.DayOfWeek == DayOfWeek.Monday ||
+                        date.DayOfWeek == DayOfWeek.Tuesday ||
+                        date.DayOfWeek == DayOfWeek.Wednesday ||
+                        date.DayOfWeek == DayOfWeek.Thursday ||
+                        date.DayOfWeek == DayOfWeek.Friday)
+                    {
+                        dates.Add(date.AddDays(-priorDays));
+                    }
+                }
+            }
+            else
+            {
+                int addDays = skipDays <= 1 ? 1 : skipDays;
+                for (DateTime date = fromDate; date <= toDate; date = date.AddDays(addDays + 1))
+                {
+                    dates.Add(date.AddDays(-priorDays));
+                }
+            }
+
+            return dates;
+        }
+
+        public static List<DateTime> GetWeeklyDatesBetweenRange(DateTime fromDate, DateTime toDate, List<DayOfWeek> days, int priorDays = 0, int skipWeeks = 0)
+        {
+            var dates = new List<DateTime>();
+            int addDays = 1;
+
+            for (DateTime date = fromDate; date <= toDate; date = date.AddDays(addDays))
+            {
+                addDays = 1;
+                if (days.Contains(date.DayOfWeek))
+                {
+                    dates.Add(date.AddDays(-priorDays));
+                }
+
+                if (skipWeeks > 0 && date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    addDays = skipWeeks * 7;
+                }
+            }
+
+            return dates;
+        }
+
+        public static List<DateTime> GetWeeklyDatesBetweenRange2(DateTime fromDate, DateTime toDate, List<DayOfWeek> days, int priorDays = 0, int skipWeeks = 0)
+        {
+            var dates = new List<DateTime>();
+            var startDate = fromDate;
+            var dateInfo = DateTimeFormatInfo.CurrentInfo;
+            int startWeek = dateInfo.Calendar.GetWeekOfYear(fromDate, CalendarWeekRule.FirstFullWeek, DayOfWeek.Monday), endWeek = 53;
+
+
+            for (int year = fromDate.Year; year <= toDate.Year; year++)
+            {
+                if (year == toDate.Year)
+                    endWeek = dateInfo.Calendar.GetWeekOfYear(toDate, CalendarWeekRule.FirstFullWeek, DayOfWeek.Monday);
+
+                for (; startWeek <= endWeek; startWeek += skipWeeks + 1)
+                {
+                    var date = new DateTime(startDate.Year, startDate.Month, startDate.Day);
+                    if (date <= toDate)
+                        dates.Add(date.AddDays(-priorDays));
+                }
+
+                if (dates.Count > 0)
+                    startWeek = dateInfo.Calendar.GetWeekOfYear(dates[dates.Count - 1], CalendarWeekRule.FirstFullWeek, DayOfWeek.Monday) + skipWeeks + 1;
+            }
+
+            return dates;
+        }
+
+        public static List<DateTime> GetMonthlyDatesBetweenRange(DateTime fromDate, DateTime toDate, int day, int priorDays = 0, int skipMonths = 0)
+        {
+            var dates = new List<DateTime>();
+            int startMonth = fromDate.Month, endMonth = 12;
+
+            for (int year = fromDate.Year; year <= toDate.Year; year++)
+            {
+                if (year == toDate.Year)
+                    endMonth = toDate.Month;
+
+                for (; startMonth <= endMonth; startMonth += skipMonths + 1)
+                {
+                    var date = new DateTime(year, startMonth, day);
+                    if (date <= toDate)
+                        dates.Add(date.AddDays(-priorDays));
+                }
+
+                if (dates.Count > 0)
+                    startMonth = dates[dates.Count - 1].AddMonths(skipMonths + 1).Month;
+            }
+
+            return dates;
+        }
+
+        public static List<DateTime> GetYearlyDatesBetweenRange(DateTime fromDate, DateTime toDate, int day, int month, int priorDays = 0)
+        {
+            var dates = new List<DateTime>();
+
+            for (int year = fromDate.Year; year <= toDate.Year; year++)
+            {
+                var date = new DateTime(year, month, day);
+
+                if (date <= toDate)
+                    dates.Add(date.AddDays(-priorDays));
+            }
+
+            return dates;
+        }
+    }
+}
---
+++ FindDatesBetweeenRangeSln/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindDatesBetweeenRangeSln
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var fromDate = new DateTime(2022, 7, 15);
+            var toDate = new DateTime(2022, 8, 5);
+            var priorDays = 0;
+            var skip = 0;
+
+            var dates = new List<DateTime>();
+
+            ///============================================ D A I L Y ==============================================
+            ///
+            //Console.WriteLine("Daily due dates between daterange without with Prior Day = 0 and Skip Days = 0.");
+            //dates = Helper.GetDailyDatesBetweenRange(fromDate, toDate, true, priorDays, skip);
+            //foreach (var date in dates)
+            //    Console.WriteLine($"Prior={priorDays} | skipDay={skip} | dueDate={date.ToString()}");
+
+            //priorDays = 1;
+            //Console.WriteLine("Daily due dates between daterange without with Prior Day = 1 and Skip Days = 0.");
+            //dates = Helper.GetDailyDatesBetweenRange(fromDate, toDate, false, priorDays, skip);
+            //foreach (var date in dates)
+            //    Console.WriteLine($"Prior={priorDays} | skipDay={skip} | dueDate={date.ToString()}");
+
+            //priorDays = 0;
+            //skip = 2;
+            //Console.WriteLine("Daily due dates between daterange without with Prior Day = 0 and Skip Days = 2.");
+            //dates = Helper.GetDailyDatesBetweenRange(fromDate, toDate, false, priorDays, skip);
+            //foreach (var date in dates)
+            //    Console.WriteLine($"Prior={priorDays} | skipDay={skip} | dueDate={date.ToString()}");
+
+            ///===================================== W E E K L Y =========================================
+            ///
+            Console.WriteLine("Weekly dates between daterange with Prior Day = 0 and Skip Week = 0.");
+            dates = Helper.GetWeeklyDatesBetweenRange(fromDate, toDate, new List<DayOfWeek> { DayOfWeek.Tuesday, DayOfWeek.Friday }, priorDays, skip);
+            foreach (var date in dates)
+                Console.WriteLine(date.ToString());
+
+            priorDays = 1;
+            Console.WriteLine("Weekly dates between daterange with Prior Day = 1 and Skip Week = 0.");
+            dates = Helper.GetWeeklyDatesBetweenRange(fromDate, toDate, new List<DayOfWeek> { DayOfWeek.Tuesday, DayOfWeek.Thursday }, priorDays, skip);
+            foreach (var date in dates)
+                Console.WriteLine(date.ToString());
+
+            ///===================================== M O N T H L Y =========================================
+            ///
+            //Console.WriteLine("Monthly dates between daterange with Prior Day = 0 and Skip months = 0.");
+            //dates = Helper.GetMonthlyDatesBetweenRange(fromDate, toDate, 18, priorDays, skip);
+            //foreach (var date in dates)
+            //    Console.WriteLine(date.ToString());
+
+            //priorDays = 1;
+            //Console.WriteLine("Monthly dates between daterange with Prior Day = 1 and Skip months = 0.");
+            //dates = Helper.GetMonthlyDatesBetweenRange(fromDate, toDate, 18, priorDays, skip);
+            //foreach (var date in dates)
+            //    Console.WriteLine(date.ToString());
+
+            //priorDays = 0;
+            //skip = 3;
+            //Console.WriteLine("Monthly dates between daterange with Prior Day = 0 and Skip months = 3.");
+            //dates = Helper.GetMonthlyDatesBetweenRange(fromDate, toDate, 18, priorDays, skip);
+            //foreach (var date in dates)
+            //    Console.WriteLine(date.ToString());
+
+            ///====================================== Y E A R L Y =======================================
+            ///
+            //Console.WriteLine("Yearly dates between daterange with Prior Day = 0 | Date = 15 | Month=3.");
+            //dates = Helper.GetYearlyDatesBetweenRange(fromDate, toDate, 1, 5, priorDays);
+            //foreach (var date in dates)
+            //    Console.WriteLine(date.ToString());
+
+            //priorDays = 1;
+            //Console.WriteLine("Yearly dates between daterange with Prior Day = 1 | Date = 15 | Month=3.");
+            //dates = Helper.GetYearlyDatesBetweenRange(fromDate, toDate, 1, 3, priorDays);
+            //foreach (var date in dates)
+            //    Console.WriteLine(date.ToString());
+        }
+    }
+}
